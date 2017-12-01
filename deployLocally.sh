60d7c865325164d3194dff74ebdfb9843d6716a6
Eicke <eicke.buecking@gmail.com>
2017-12-01 13:43:18 +0100

    Added Docker Deployment-Script

--- deployLocally.sh
+++ deployLocally.sh
@@ -1,2 +1,2 @@
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=eicke74/authenticationserver --format="{{.ID}}"))
-docker run -t --net host eicke74/authenticationserver
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=eicke74/travelmanager --format="{{.ID}}"))
+docker run -t --net host eicke74/travelmanager
