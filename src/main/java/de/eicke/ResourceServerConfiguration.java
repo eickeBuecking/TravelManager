51654b8149a8b1061e12264ef2508915a97ae4ac
Eicke <eicke.buecking@gmail.com>
2017-12-20 12:40:06 +0100

    Implement Service Call

--- src/main/java/de/eicke/ResourceServerConfiguration.java
+++ src/main/java/de/eicke/ResourceServerConfiguration.java
@@ -1,27 +1,59 @@
 package de.eicke;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
  
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
  
-    private static final String RESOURCE_ID = "my_rest_api";
-     
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) {
-        resources.resourceId(RESOURCE_ID).stateless(false);
-    }
+//    private static final String RESOURCE_ID = "my_rest_api";
+//     
+//    @Override
+//    public void configure(ResourceServerSecurityConfigurer resources) {
+//        resources.resourceId(RESOURCE_ID).stateless(false);
+//        resources.tokenServices(tokenServices());
+//    }
+// 
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//    	http.httpBasic().disable().
+//            authorizeRequests()             
+//            	.anyRequest().hasRole("USER");
+//   }
+
  
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-    	http.httpBasic().disable().
-            authorizeRequests()             
-            	.anyRequest().hasRole("USER");
+    public void configure(ResourceServerSecurityConfigurer config) {
+        config.tokenServices(tokenServices());
+    }
+ 
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+ 
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey("123");
+        return converter;
+    }
+ 
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        return defaultTokenServices;
     }
 }
